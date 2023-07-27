eb3b832364beee08b3f11532fee9fa38a9f2aab3
SATAd15k <140038492+SATAd15k@users.noreply.github.com>
2023-07-27 12:47:38 +1000

    mORE things for layout

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -21,7 +21,41 @@ public class Grid : MonoBehaviour
         // GUI.Box(new Rect(0, 0, 100, 50), "HELLO WORLD!");
 
 
-        // Scaling Box by aspect ratio
-        GUI.Box(new Rect(Screen.width / 16, Screen.height / 9, Screen.width / 16, Screen.width / 9), "Hello World");
+        // Scaling Box by aspect ratio defining the scale inline
+        // GUI.Box(new Rect(Screen.width / 16 * 0, Screen.height / 9 * 3, Screen.width / 16 * 1, 0.5f*Screen.height / 9 * 1), "Hello World");
+
+        // This new GUI.Box doesnt need to have width and height as well as box sizes as it has been defined globally / top level
+
+        GUI.Box(new Rect(ScreenPlacement(2.5f, 3, 2, 4)), "helloz");
+        LessonGrid();
+    }
+
+    // This is known as a method which does not have a return type; doesnt have to give values will run top to bottom
+    // void does not return a type aka null
+    void LessonGrid()
+    {
+        /*
+         Loops, For, For Each, While, Do While
+        */
+        // i = iteratation; default count up
+
+        for (int x = 0; x < 16; x++)
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                GUI.Box(new Rect(ScreenPlacement(x, y, 1, 1)), "");
+                GUI.Label(new Rect(ScreenPlacement(x, y, 1, 1)), x + ":" + y);
+            }
+        }
+    }
+    // This is known as a function (or behavior) which has a return type; has to give something back which can be used elsewhere
+    public Rect ScreenPlacement(float startPosX, float startPosY, float sizeX, float sizeY)
+    {
+        // Defining how Rect scaling values sit when called in other nested scripts as this is top level *GOD SCRIPT*
+        Rect placement = new Rect(startPosX*Screen.width / 16, startPosY*Screen.height / 9, sizeX*Screen.width / 16, sizeY*Screen.height / 9);
+        return placement;
+
+        // In this case it will return value a new rectangle which will be defined by the values above
+        return new Rect();
     }
 }
